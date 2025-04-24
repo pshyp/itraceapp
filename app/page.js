882c6736420d58a30c2a094af741e1b61fadb8c9
Peter Kamau Ndung'u <ppeterkamaundungu@gmail.com>
2025-04-24 18:56:00 +0300

    page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,27 @@
-// app/page.js
+import Image from "next/image";
+import "./globals.css";
+
 export default function Home() {
   return (
-    <div>
-      <h2>Welcome to iTrace!</h2>
-      <p>This is your homepage content.</p>
+    <div className="fuel-home-container">
+      <div className="fuel-home-content">
+        <h1>Fuel Monitor FTC 921</h1>
+        <p>Built for Precision Fuel Intelligence.</p>
+
+        <div className="image-wrapper">
+          <Image
+            src="/FTC921.webp"
+            alt="Fuel Monitor FTC 921"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+
+        <div className="buttons">
+          <button className="primary">Learn more</button>
+          <button className="secondary">Buy</button>
+        </div>
+      </div>
     </div>
   );
 }
