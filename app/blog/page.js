4eeb348f8f2f85d12483471ddba95b384be42394
ppeterkamaundungu <ppeterkamaundungu@gmail.com>
2025-05-13 07:38:29 +0000

    blog

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,10 +1,41 @@
-const BlogPage = () => {
+'use client';
+
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const BlogPage = ({ posts }) => {
   return (
     <div>
-      <h1>Blog</h1>
-      <p>This is the blog page content.</p>
+      <h1>All Blog Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link href={`/blog/${post.slug}`}>
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const postsDirectory = path.join(process.cwd(), 'content', 'blog');
+  const filenames = fs.readdirSync(postsDirectory);
+
+  const posts = filenames.map((filename) => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const { data } = matter(fileContents);
+    const slug = filename.replace(/\.mdx$/, '');
+    return { slug, ...data };
+  });
+
+  return { props: { posts } };
+}
 
 export default BlogPage;
