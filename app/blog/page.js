1f1d8ea565822eec5df6a9dbc13eb5c656ede3a1
ppeterkamaundungu <ppeterkamaundungu@gmail.com>
2025-05-15 21:57:40 +0000

    bloo

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,43 +1,15 @@
 'use client';
 
 import Link from 'next/link';
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import { useEffect, useState } from 'react';
+
 
 const BlogPage = () => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const postsDirectory = path.join(process.cwd(), 'content', 'blog');
-      const filenames = fs.readdirSync(postsDirectory);
-
-      const blogPosts = filenames.map((filename) => {
-        const filePath = path.join(postsDirectory, filename);
-        const fileContents = fs.readFileSync(filePath, 'utf8');
-        const { data } = matter(fileContents);
-        const slug = filename.replace(/\.mdx$/, '');
-        return { slug, ...data };
-      });
-      setPosts(blogPosts);
-    };
-    fetchPosts();
-  }, []);
+ 
 
   return (
     <div>
       <h1>All Blog Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link href={`/blog/${post.slug}`}>
-              {post.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      
     </div>
   );
 };
