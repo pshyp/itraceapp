04f9b32a3e8633807e3537b350833a8c6276c146
ppeterkamaundungu <ppeterkamaundungu@gmail.com>
2025-05-19 23:03:29 +0000

    kik

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,10 +1,26 @@
 'use client';
 
-import Link from 'next/link';
-
+import Link from "next/link";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { useState, useEffect } from "react";
 
 const BlogPage = () => {
- 
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const postsDirectory = path.join(process.cwd(), "content/blog");
+    const filenames = fs.readdirSync(postsDirectory);
+
+    const postsData = filenames.map((filename) => {
+      const filePath = path.join(postsDirectory, filename);
+      const fileContent = fs.readFileSync(filePath, "utf-8");
+      const { data, content } = matter(fileContent);
+      return { slug: filename.replace(".mdx", ""), ...data };
+    });
+    setPosts(postsData);
+  }, []);
 
   return (
     <div>
