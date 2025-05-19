3c1bf33cdd601a3ba0599d8623099b1738ff397b
ppeterkamaundungu <ppeterkamaundungu@gmail.com>
2025-05-19 23:14:30 +0000

    mm

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,35 +1,10 @@
-import fs from "fs";
-import path from "path";
-import Link from 'next/link';
-import matter from "gray-matter";
-
-async function getBlogPosts() {
-  const postsDirectory = path.join(process.cwd(), "content/blog");
-  const filenames = fs.readdirSync(postsDirectory);
-  const postsData = filenames.map((filename) => {
-    const filePath = path.join(postsDirectory, filename);
-    const fileContent = fs.readFileSync(filePath, "utf-8");
-    const { data } = matter(fileContent);
-    return { slug: filename.replace(".mdx", ""), ...data };
-  });
-  return postsData;
-}
-
-const BlogPage = async () => {
-  const posts = await getBlogPosts();
-
+const BlogPage = () => {
   return (
     <div>
       <h1>All Blog Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            {/* Make the title clickable and link to the blog post */}
-            <Link href={/blog/${post.slug}}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <p>This is a placeholder blog page.</p>
     </div>
   );
 };
+
 export default BlogPage;
