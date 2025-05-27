7cefaa156c16ddf80e73818c475eb89c78ce0b0e
ppeterkamaundungu <ppeterkamaundungu@gmail.com>
2025-05-27 16:41:16 +0000

    g

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,10 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
 const BlogPage = () => {
+  const blogDirectory = path.join(process.cwd(), 'content', 'blog');
+  const filenames = fs.readdirSync(blogDirectory);
+
+  const posts = filenames.map((filename) => {
+    const filePath = path.join(blogDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const { data } = matter(fileContents);
+
+    return {
+ title: data.title,
+ date: data.date,
+ image: data.image, // Assuming you add an 'image' field to your frontmatter
+ description: data.description, // You might want to add a description field to your frontmatter as well
+      slug: filename.replace('.mdx', ''),
+    };
+  });
+
   return (
     <div>
       <h1>All Blog Posts</h1>
-      <p>This is a placeholder blog page.</p>
+      <div className="blog-posts-container">
+        {posts.map((post) => (
+          <div key={post.slug} className="blog-card">
+            <h2>{post.title}</h2>
+ {post.image && <img src={post.image} alt={post.title} />}
+            <p>{post.date}</p>
+            <p>{post.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-
 export default BlogPage;
