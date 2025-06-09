505e8670ed609b46bcf2b9a3dc37dbdc5318d4bb
ppeterkamaundungu <ppeterkamaundungu@gmail.com>
2025-06-09 11:29:29 +0000

    Make whatsapp banner float

--- app/fuel-monitoring/page.js
+++ app/fuel-monitoring/page.js
@@ -1,12 +1,12 @@
 "use client";
 
 import styles from "./fuel-monitoring.module.css";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const FuelMonitoringPage = () => {
   const [visibleBenefitIndex, setVisibleBenefitIndex] = useState(0);
 
-  const benefits = [    
+  const benefits = [
     "Reduced cost of fuel.",
     "Optimize route and monitor driver behavior.",
     "Prevent fuel theft with advanced fuel sensors.",
@@ -15,98 +15,70 @@ const FuelMonitoringPage = () => {
   ];
 
   const imageFiles = [
-    { src: '/fmc125.webp', name: 'FMC125', description: "FMC125 fuel monitoring device" },
-    { src: '/tat140.webp', name: 'TAT140', description: "TAT140 fuel monitoring device" },
-    { src: '/GH5200.png', name: 'GH5200', description: "GH5200 fuel monitoring device" },
-    { src: '/FTC921.png', name: 'FTC921' },
-    { src: '/FTC961.png', name: 'FTC961' },
-    { src: '/FTC881.png', name: 'FTC881' },
-    { src: '/fmc920.webp', name: 'FMC920' },
-    { src: '/fmc650.webp', name: 'FMC650' },
-    { src: '/fmm250.webp', name: 'FMM250' },
+    { src: "/fmc125.webp", name: "FMC125", description: "FMC125 fuel monitoring device" },
+    { src: "/tat140.webp", name: "TAT140", description: "TAT140 fuel monitoring device" },
+    { src: "/GH5200.png", name: "GH5200", description: "GH5200 fuel monitoring device" },
+    { src: "/FTC921.png", name: "FTC921" },
+    { src: "/FTC961.png", name: "FTC961" },
+    { src: "/FTC881.png", name: "FTC881" },
+    { src: "/fmc920.webp", name: "FMC920" },
+    { src: "/fmc650.webp", name: "FMC650" },
+    { src: "/fmm250.webp", name: "FMM250" },
   ];
 
   const deviceDetails = {
     FMC125: {
-      name: 'FMC125',
-      intro: 'Advanced Fuel Monitoring',
-      features: [
-        'Fuel monitoring and more',
-        'Significantly lower roaming costs',
-        'Accurate fuel usage control',
-      ],
+      name: "FMC125",
+      intro: "Advanced Fuel Monitoring",
+      features: ["Fuel monitoring and more", "Significantly lower roaming costs", "Accurate fuel usage control"],
     },
     TAT140: {
-      name: 'TAT140',
-      intro: 'Simple & Reliable Tracking',
-      features: [
-        'Periodic location reports',
-        'Backup tracking the easy way',
-        'Extendable battery life',
-      ],
+      name: "TAT140",
+      intro: "Simple & Reliable Tracking",
+      features: ["Periodic location reports", "Backup tracking the easy way", "Extendable battery life"],
     },
     GH5200: {
-      name: 'GH5200',
-      intro: 'Personal Safety & Communication',
-      features: [
-        'Two-way voice communication',
-        'Programmable buttons',
-        'Alarm button',
-      ],
+      name: "GH5200",
+      intro: "Personal Safety & Communication",
+      features: ["Two-way voice communication", "Programmable buttons", "Alarm button"],
     },
     FTC921: {
-      name: 'FTC921',
-      intro: 'Robust Fleet Tracking',
-      features: [
-        'FT platform device',
-        'High voltage power supply',
-        'Longer performance, slower drainage',
-      ],
+      name: "FTC921",
+      intro: "Robust Fleet Tracking",
+      features: ["FT platform device", "High voltage power supply", "Longer performance, slower drainage"],
     },
     FTC961: {
-      name: 'FTC961',
-      intro: 'Heavy-Duty Tracking',
-      features: [
-        'FT platform device',
-        'IP69K waterproof casing',
-        'High voltage power supply',
-      ],
+      name: "FTC961",
+      intro: "Heavy-Duty Tracking",
+      features: ["FT platform device", "IP69K waterproof casing", "High voltage power supply"],
     },
     FTC881: {
-      name: 'FTC881',
-      intro: 'Easy Installation Tracking',
-      features: [
-        'FT platform device',
-        'Fast installation',
-        'High voltage power supply',
-      ],
+      name: "FTC881",
+      intro: "Easy Installation Tracking",
+      features: ["FT platform device", "Fast installation", "High voltage power supply"],
     },
     FMC920: {
-      name: 'FMC920',
-      intro: 'Smart & Efficient Tracking',
+      name: "FMC920",
+      intro: "Smart & Efficient Tracking",
       features: [
-        'Eco driving scenario for all-round better driving',
-        'Slim design to fit the tightest spaces',
-        'Extensive remote monitoring possibilities',
+        "Eco driving scenario for all-round better driving",
+        "Slim design to fit the tightest spaces",
+        "Extensive remote monitoring possibilities",
       ],
     },
     FMC650: {
-      name: 'FMC650',
-      intro: 'Comprehensive Vehicle Data',
+      name: "FMC650",
+      intro: "Comprehensive Vehicle Data",
       features: [
-        'Reliable global coverage and separate GNSS module',
-        'Remote download of Tachograph files and live data',
-        'CAN data reading from heavy vehicles and special machinery',
+        "Reliable global coverage and separate GNSS module",
+        "Remote download of Tachograph files and live data",
+        "CAN data reading from heavy vehicles and special machinery",
       ],
     },
     FMM250: {
-      name: 'FMM250',
-      intro: 'EV & CAN Bus Tracking',
-      features: [
-        'Protection against dust and water',
-        'Built-in CAN bus data reading chip',
-        'CAN data reading from EVs',
-      ],
+      name: "FMM250",
+      intro: "EV & CAN Bus Tracking",
+      features: ["Protection against dust and water", "Built-in CAN bus data reading chip", "CAN data reading from EVs"],
     },
   };
 
@@ -114,7 +86,6 @@ const FuelMonitoringPage = () => {
     const interval = setInterval(() => {
       setVisibleBenefitIndex((prevIndex) => (prevIndex + 1) % benefits.length);
     }, 4000);
-
     return () => clearInterval(interval);
   }, []);
 
@@ -132,13 +103,20 @@ const FuelMonitoringPage = () => {
       <div className={styles.headerRow}>
         <h1 className={styles.fuelMonitoringTitle}>
           Fuel Monitoring, GPS Tracking & Fleet Management Solutions
-                  </h1>
- {/* Add the WhatsApp link here */}
+        </h1>
+
         <div className={styles.whatsappLinkContainer}>
-          <a href="https://wa.me/254722100506" target="_blank" rel="noopener noreferrer" className={styles.whatsappLink}>
-            Chat on WhatsApp
+          <a
+            href="https://wa.me/254722100506"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.whatsappLink}
+          >
+            <img src="/whatsapp-icon.png" alt="WhatsApp" className={styles.whatsappIcon} />
+            Chat with Us on WhatsApp
           </a>
         </div>
+
         <div className={styles.benefitsSection}>
           <h2 className={styles.keyFeaturesTitle}>Key Benefits</h2>
           <ul className={styles.keyFeaturesList}>
@@ -163,14 +141,16 @@ const FuelMonitoringPage = () => {
             return (
               <div key={index} className={styles.imageItem}>
                 {details?.intro && (
-                  <p className={`${styles.deviceIntro} ${styles.initialHidden}`}>
-                    {details.intro}
-                  </p>
+                  <p className={`${styles.deviceIntro} ${styles.initialHidden}`}>{details.intro}</p>
                 )}
                 <div className={styles.imageContainer}>
                   <img
                     src={image.src}
-                    alt={details?.name ? `Fuel monitoring device ${details.name} details and features` : `Fuel monitoring device ${image.description}`}
+                    alt={
+                      details?.name
+                        ? `Fuel monitoring device ${details.name} details and features`
+                        : `Fuel monitoring device ${image.description}`
+                    }
                     className={styles.responsiveImage}
                   />
                 </div>
