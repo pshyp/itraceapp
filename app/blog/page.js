2aae7c4034b22dc0b1855e8e5f0d06341a60ada3
ppeterkamaundungu <ppeterkamaundungu@gmail.com>
2025-09-13 21:31:34 +0000

    fix: blog post title color

--- app/blog/page.js
+++ app/blog/page.js
@@ -2,20 +2,29 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./blog.module.css";
 
 function getBlogs() {
   const contentDir = path.join(process.cwd(), "public/content");
   const filenames = fs.readdirSync(contentDir).filter((file) => file.endsWith(".mdx"));
 
+  const publicDir = path.join(process.cwd(), "public");
+  const images = fs.readdirSync(publicDir).filter((file) =>
+    /\.(png|jpg|jpeg|webp)$/i.test(file)
+  );
+
   const blogs = filenames.map((filename) => {
     const filePath = path.join(contentDir, filename);
     const fileContents = fs.readFileSync(filePath, "utf8");
     const { data } = matter(fileContents);
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+
     return {
       slug: data.slug || filename.replace(/\.mdx$/, ""),
       title: data.title || "Untitled",
       author: data.author || "Anonymous",
+      image: `/${randomImage}`,
     };
   });
   return blogs;
@@ -29,6 +38,7 @@ export default function Blog() {
       <div className={styles.blogGrid}>
         {blogs.map((blog) => (
           <div key={blog.slug} className={styles.blogCard}>
+            <Image src={blog.image} alt={blog.title} width={300} height={200} />
             <h2>{blog.title}</h2>
             <p className={styles.blogAuthor}>By {blog.author}</p>
             <Link href={`/blog/${blog.slug}`}>Read More</Link>
