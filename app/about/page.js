47d3b73be20b4732487b71c7498920eb6e8b4373
ppeterkamaundungu <ppeterkamaundungu@gmail.com>
2025-09-14 22:02:32 +0000

    feat(performance): Lazy load partner logos on about page

--- app/about/page.js
+++ app/about/page.js
@@ -37,6 +37,7 @@ export default function AboutPage() {
  width={100}
  height={50}
  className="partner-logo"
+ loading="lazy"
  />
  <Image
  src="/EMKFKenya.png"
@@ -44,6 +45,7 @@ export default function AboutPage() {
  width={100}
  height={50}
  className="partner-logo"
+ loading="lazy"
  />
  <Image
  src="/KCIC.png"
@@ -51,6 +53,7 @@ export default function AboutPage() {
  width={100}
  height={50}
  className="partner-logo"
+ loading="lazy"
  />
  <Image
  src="/Mojaev.png"
@@ -58,6 +61,7 @@ export default function AboutPage() {
  width={100}
  height={50}
  className="partner-logo"
+ loading="lazy"
  />
 
 
